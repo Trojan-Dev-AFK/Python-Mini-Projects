b46f4fb4f31416cc37ca13945a42aa37aadbe9c2
Trojan0101 <aloysius.mon.05@gmail.com>
2024-05-26 09:08:00 +0530

    Signature detection

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     # Signature Detection
     signature_detector = SignatureDetector('./output_models/best_signature_vs_text_model.h5')
-    signature_results = signature_detector.process_images('./example_data/output3/timestamps/')
+    signature_results = signature_detector.process_images('./example_data/output3/signs/')
     # Save the results to a JSON file
     output_file = './output_json/signature_results.json'
     signature_detector.save_results_to_json(signature_results, output_file)
